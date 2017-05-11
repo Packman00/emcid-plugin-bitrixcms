0a8e2d878d1d2f27d926f79c606b79906e939497
Vladimir Stempel <v.stempel@aspanta.com>
2017-05-11 12:29:56 +0300

    Module name fix

--- emercoinid/lang/en/install/index.php
+++ emercoinid/lang/en/install/index.php
@@ -1,5 +1,5 @@
 <?
-$MESS["emercoinid_install_name"] = "Authorisation with Emercoin ID";
+$MESS["emercoinid_install_name"] = "Authorization with Emercoin ID";
 $MESS["emercoinid_install_desc"] = "Integration with Emercoin ID.";
 $MESS["emercoinid_install_title_inst"] = "Authorisation with Emercoin ID Module Installation";
 $MESS["emercoinid_install_title_unitst"] = "Authorisation with Emercoin ID Module Uninstallation";
