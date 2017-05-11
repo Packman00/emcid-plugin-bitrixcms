bbc1d71a1e47e309e812817cd93db7556f6be48b
Vladimir Stempel <v.stempel@aspanta.com>
2017-05-11 12:59:14 +0300

    Added fields description, fixed language files

--- emercoinid/lang/en/install/index.php
+++ emercoinid/lang/en/install/index.php
@@ -1,6 +1,6 @@
 <?
 $MESS["emercoinid_install_name"] = "Authorization with Emercoin ID";
 $MESS["emercoinid_install_desc"] = "Integration with Emercoin ID.";
-$MESS["emercoinid_install_title_inst"] = "Authorisation with Emercoin ID Module Installation";
-$MESS["emercoinid_install_title_unitst"] = "Authorisation with Emercoin ID Module Uninstallation";
+$MESS["emercoinid_install_title_inst"] = "Authorization with Emercoin ID Module Installation";
+$MESS["emercoinid_install_title_unitst"] = "Authorization with Emercoin ID Module Uninstallation";
 ?>
--- emercoinid/lang/ru/classes/general/emercoinid.php
+++ emercoinid/lang/ru/classes/general/emercoinid.php
@@ -1,7 +1,11 @@
 <?
+<?
 $MESS["socserv_emercoinid_authurl"] = "Страница авторизации (Auth Page):";
+$MESS["socserv_emercoinid_authurl_note"] = "Emercoin ID Страница авторизации (Например: https://id.emercoin.net/oauth/v2/auth)";
 $MESS["socserv_emercoinid_tokenurl"] = "Страница токена (Token Page):";
+$MESS["socserv_emercoinid_tokenurl_note"] = "Emercoin ID Страница токена (Например: https://id.emercoin.net/oauth/v2/token)";
 $MESS["socserv_emercoinid_accounturl"] = "Инфо (Infocard Page):";
+$MESS["socserv_emercoinid_accounturl_note"] = "Emercoin ID Инфо (Например: https://id.emercoin.net/infocard)";
 $MESS["socserv_emercoinid_client_id"] = "Идентификатор (App key):";
 $MESS["socserv_emercoinid_client_secret"] = "Секретный код (App secret):";
 $MESS["socserv_emercoinid_note"] = "Для получения идентификаторов необходимо <a href=\"https://oauth.authorizer.io/\">создать приложение</a> типа \"Emercoin ID API app\". <br>После создания в настройках приложения в поле \"Redirect URIs\" укажите адрес: <a href=\"#URL#\">#URL#</a>.<br />Поддерживается только https.";
